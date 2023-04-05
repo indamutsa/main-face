c752492f84ee6dfd6a9edce30198b8b793e377a1
indamutsa <arsichizy@gmail.com>
2023-04-05 14:51:11 +0200

    First commit

---
+++ frontend/src/index.js
@@ -0,0 +1,11 @@
+
+// This is the entry point for the React app. It renders the App component
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Import the App component
+import App from './App'
+import './index.css'
+
+// Render the App component in the DOM
+ReactDOM.render(<App />, document.getElementById('root'))
